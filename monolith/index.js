307faca4f94626eaeb2b64ad3802f813f1eeba60
y0ng0p3 <vaillantyomen@gmail.com>
2023-03-06 16:33:14 +0100

    Monolith as a subgraph.

--- monolith/index.js
+++ monolith/index.js
@@ -1,40 +1,45 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { buildSubgraphSchema } = require("@apollo/subgraph");
 
-const { readFileSync } = require('fs');
-const axios = require('axios');
-const gql = require('graphql-tag');
+const { readFileSync } = require("fs");
+const axios = require("axios");
+const gql = require("graphql-tag");
 
-const { AuthenticationError } = require('./utils/errors');
+const { AuthenticationError } = require("./utils/errors");
 
-const typeDefs = gql(readFileSync('./schema.graphql', { encoding: 'utf-8' }));
-const resolvers = require('./resolvers');
+const typeDefs = gql(readFileSync("./schema.graphql", { encoding: "utf-8" }));
+const resolvers = require("./resolvers");
 
-const BookingsDataSource = require('./datasources/bookings');
-const ReviewsDataSource = require('./datasources/reviews');
-const ListingsAPI = require('./datasources/listings');
-const AccountsAPI = require('./datasources/accounts');
-const PaymentsAPI = require('./datasources/payments');
+const BookingsDataSource = require("./datasources/bookings");
+const ReviewsDataSource = require("./datasources/reviews");
+const ListingsAPI = require("./datasources/listings");
+const AccountsAPI = require("./datasources/accounts");
+const PaymentsAPI = require("./datasources/payments");
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema: buildSubgraphSchema({
+      typeDefs,
+      resolvers,
+    }),
   });
 
-  const port = 4000;
+  const port = 4001;
 
   try {
     const { url } = await startStandaloneServer(server, {
       context: async ({ req }) => {
-        const token = req.headers.authorization || '';
-        const userId = token.split(' ')[1]; // get the user name after 'Bearer '
+        const token = req.headers.authorization || "";
+        const userId = token.split(" ")[1]; // get the user name after 'Bearer '
 
         let userInfo = {};
         if (userId) {
-          const { data } = await axios.get(`http://localhost:4011/login/${userId}`).catch((error) => {
-            throw AuthenticationError();
-          });
+          const { data } = await axios
+            .get(`http://localhost:4011/login/${userId}`)
+            .catch((error) => {
+              throw AuthenticationError();
+            });
 
           userInfo = { userId: data.id, userRole: data.role };
         }
